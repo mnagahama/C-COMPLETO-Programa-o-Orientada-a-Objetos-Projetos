ac3899bbe03e8b32ac125c178ba24cdd3db95d4a
mnagahama <mnagahama21@gmail.com>
2023-03-29 19:43:36 -0300

    Exercicio

---
+++ Secao03/ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o seu nome completo: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa ?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu ultimo nome, idade e altura (mesma linha): ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string lastName = vet[0];
+            int age = int.Parse(vet[1]);
+            double height = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
