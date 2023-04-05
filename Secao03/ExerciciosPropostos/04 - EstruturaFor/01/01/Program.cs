238323988e944c1c6955f34343f8c612eebe7078
mnagahama <mnagahama21@gmail.com>
2023-04-04 21:44:30 -0300

    Exercicio estrutura for

---
+++ Secao03/ExerciciosPropostos/04 - EstruturaFor/01/01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i =1; i<= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Secao03/ExerciciosPropostos/04 - EstruturaFor/02/02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o intervalo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int intervalo_in = 0;
+            int intervalo_out = 0;
+
+            for (int i=0; i <=n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    intervalo_in++;
+                }
+
+                else
+                {
+                    intervalo_out++;
+                }
+                
+            }
+
+            Console.WriteLine(intervalo_in + " in");
+            Console.WriteLine(intervalo_out + " out");
+        }
+    }
+}
